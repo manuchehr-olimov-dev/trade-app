60ba2ec873964171143f30bc3aa4a13f953a9785
manuchehr-olimov-dev <mancuhehr.dev@gmail.com>
2024-03-11 21:36:34 +0300

    Done

--- components/deals.py
+++ components/deals.py
@@ -1,5 +1,6 @@
 import json
 
+import psycopg2
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.toolbar import MDBottomAppBar, MDActionBottomAppBarButton
@@ -19,4 +20,32 @@ class Deals(Screen):
 
     def __init__(self, **kwargs):
         super(Deals, self).__init__(**kwargs)
+
+    def show_deals(self):
+        self.ids.dealsContainer.clear_widgets()
+        connection = psycopg2.connect(
+            database="trader-db",
+            user="postgres",
+            password="1",
+            host="localhost",
+            port=5432
+        )
+
+        if (connection):
+            print("ok")
+        cursor = connection.cursor()
+        postgreSQL_select_Query = "select * from deals"
+
+        cursor.execute(postgreSQL_select_Query)
+        deals = cursor.fetchall()
+
+        print("Вывод каждой строки и ее столбцов")
+        for deal in deals:
+            self.ids.dealsContainer.add_widget(
+                ThreeLineListItem(
+                    text=f"ID: {deal[0]}",
+                    secondary_text=f"Тикет: {deal[1]}",
+                    tertiary_text=f"Дата: {deal[3]}"
+                )
+            )
 
